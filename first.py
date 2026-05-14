cf54e8448b2d15aeadeaf41ffd8310764f033e2b
Pranisha Shrestha <082bel021@ioepc.edu.np>
2026-05-14 15:07:45 +0545

    i/o

--- first.py
+++ first.py
@@ -1,2 +1,11 @@
-nam = input("Enter a number: ")
-print(nam)
+num = 33.4
+hi = 22.9
+name = "pranisha"
+aa = "13.5"
+aa = float(aa)
+new = int(hi + num)
+print("the number is : ", new)
+print("data type:", type(new))
+print("name is :", name)
+print("data type:", type(name))
+print(aa)
---
+++ io.py
@@ -0,0 +1,14 @@
+'''print("good", end=' ')
+print('morning')
+#good, morning, welcome etc they are objects.
+#end="" print vayesi last ma k hunxa eg, \n, whitespace, tab, etc
+#sep, objects lai seperate garna ko lagi use hunxa, default ma space hunxa
+print('welcome', 'to', 'python', sep='+')
+'''
+# print(5) #5 is a literal
+name = input("ur name is? ")
+print("hi ", name)
+n1= input("enter num1: ")
+n2= input("enter num2: ")
+add= int(n1) + int(n2)
+print("the sum is: ", add)
