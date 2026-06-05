e37b969ad559e91ed72c140b76bab229ce140a88
Pranisha Shrestha <082bel021@ioepc.edu.np>
2026-06-05 20:14:39 +0545

    lab1

---
+++ lab1/swap.py
@@ -0,0 +1,7 @@
+n1 = int(input("ENter no.1: "))
+n2 = int(input("ENter no.2: "))
+print(f"Before swapping: first number is {n1} and second number is {n2}.")
+temp = n1
+n1 = n2 
+n2 = temp
+print(f"After swapping: first number is {n1} and second number is {n2}.")
