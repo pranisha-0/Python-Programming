08689ab67684bc0adcde2ad4002b8a0f6d2c93af
Pranisha Shrestha <082bel021@ioepc.edu.np>
2026-06-22 00:23:23 +0545

    hell

--- practice_a/patterns.py
+++ practice_a/patterns.py
@@ -1,11 +1,21 @@
-i = 1
-star = '* '
-while i <= 5:
-    print(star)
-    i = i + 1
-    star += '* '
-j = 5
-while j >= 1:
-    print('* ' * j)
-    j = j - 1
-    
+'''#10 ota star
+print("* "*10)
+print("\n")
+
+'''#star box
+'''star box
+for i in range(0, 5):
+    print("* "*5)
+print("\n")'''
+
+#left triangle
+for i in range (0, 6):
+    print("* "*i)
+
+#left upside down triangle
+for i in range (6, 0, -1):
+    print("* "*i)
+
+#right triangle
+for i in range(0, 6):
+    print(" "*(6-i) + "*"*i)
