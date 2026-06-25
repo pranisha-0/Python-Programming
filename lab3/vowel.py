e9f2c7a7dc43b3ec8f247e3c401f28f47127406b
Pranisha Shrestha <082bel021@ioepc.edu.np>
2026-06-25 20:46:13 +0545

    k

--- lab3/vowel.py
+++ lab3/vowel.py
@@ -1,8 +1,21 @@
 #vowel or consonant check
-let = input("Enter a letter: ")
+'''let = input("Enter a letter: ")
 if let == "a" or let == "e" or let == "i" or let == "o" or let == "u":
     print(f"{let} is a vowel.")
 elif let == "A" or let == "E" or let == "I" or let == "O" or let == "U":
         print(f"{let} is a vowel.")
 else:
-    print(f"{let} is a consonant.")
+    print(f"{let} is a consonant.")'''
+
+ch = input("Enter a character: ")
+
+a = ord(ch)
+
+if (65 <= a <= 90) or (97 <= a <= 122):
+    if a == 65 or a == 69 or a == 73 or a == 79 or a == 85 or a == 97 or a == 101 or a == 105 or a == 111 or a == 117:
+        print("Vowel")
+    else:
+        print("Consonant")
+else:
+    print("Not an alphabet")
+    
