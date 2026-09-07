bfdcf70043f41ee6284b8405ea6d7f6e8867b2e2
Pranisha Shrestha <082bel021@ioepc.edu.np>
2026-09-07 23:44:10 +0545

    more numpy

--- practice_a/numpy_pra.py
+++ practice_a/numpy_pra.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-print(np.__version__)
+print(np.__version__) #numpy ko version check garna
 #1D array and indexing slicing
 a = np.array([1, 2, 3, 4, 6])
 print(a[1])
@@ -19,12 +19,15 @@ print(b[:2, 1:2])
 
 #print(np.zeros(3, 4))
 
-c = a
+c = a #copies a to c but dependntly i.e if c changes, a will also change
 c[0] = 100
 print(c[0])
 print(a)
-d = c.copy()
+d = c.copy() #copies array c to d independently i.e if d changes c wont change
 print(d)
 
-a[a>10] = 7
-print(a)
+a[a>10] = 7 #chhanges every number in the array which is greater than 10 to 7
+print(a) 
+
+b[1, 1] = 50
+print(b)
